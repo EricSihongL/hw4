ee9feeddec72bfc9e9588298539df979fcd3c018
EricSihongL <31781716+EricSihongL@users.noreply.github.com>
2017-10-02 00:14:16 -0700

    Create sketch-5.js

---
+++ sketch-5.js
@@ -0,0 +1,19 @@
+var y=0;
+var x1=170;
+var x2=175;
+
+while (y<=170){
+  line(x1,y,x2,y);
+  x1=x1-10;
+  x2=x2+10;
+  y=y+10;
+}
+while (y>=170);{
+  	line(x1,y,x2,y);
+   	x1=x1+10;
+    x2=x2-10;
+    y=y-10;
+}
+  
+
+
