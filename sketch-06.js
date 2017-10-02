613e7ea87f32097dfd7a3c5c2e7e8b32c632d93f
EricSihongL <31781716+EricSihongL@users.noreply.github.com>
2017-10-02 15:15:57 -0700

    Create sketch-06.js

---
+++ sketch-06.js
@@ -0,0 +1,16 @@
+
+rectMode(CENTER);
+var x=50;
+var y=170;
+var h=15;
+var w=15;
+
+while (h<=45){
+  rect(x,y,h,w);
+  noFill();
+       h=h+10;
+       w=w+10;
+  if (h<=45);
+  h=h-40;
+  x=x+50;
+}
